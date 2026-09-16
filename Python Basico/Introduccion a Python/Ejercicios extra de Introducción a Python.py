fb0f80120089fb81d447f6d2fd9524f38dbc9d22
Francisco <francordero13@gmail.com>
2026-09-15 18:10:24 -0600

    New changes uploaded

---
+++ Python Basico/Introduccion a Python/Ejercicios extra de Introducción a Python.py
@@ -0,0 +1,11 @@
+print("Mi nombre es Francisco Cordero")
+print("Tengo 28 años")
+print("Mi color favorito es el negro")
+print("Mi comida favorita es la pasta en salsa blanca")
+
+
+
+print("Dentro de 10 años yo voy a tener",10+28)
+
+
+print("15 metros son",15*100,"centimetros")
